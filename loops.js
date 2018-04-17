6ff4d2b51bdf1acba677d82ad8d363295ed4bc22
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-04-17 20:08:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,13 +2,7 @@ function forLoop(array) {
   
 
 for (let i = 0; i < 25; i++) {
-    if(i===0) {
-      `I am ${i} strange loop.`
-    } else if(i===1) {
-      "I am 1 strange loop."
-    } else {
-      `I am ${i} strange loops.`
-    }
+    
     
   }
 
